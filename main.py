01e2a1a4ab50aa8d25042680ca57adcad772448c
stman3 <75269916+stman3@users.noreply.github.com>
2022-07-11 07:03:32 +0300

    Pipe class

--- main.py
+++ main.py
@@ -106,6 +106,31 @@ class Pipe:
         self.top = self.height - self.PIPE_TOP.get_height()
         self.bottom = self.height + self.GAP
 
+    def move(self):
+        self.x -= self.VEL
+
+    def draw(self,win):
+        win.blit(self.PIPE_TOP,(self.x,self.top))
+        win.blit(self.PIPE_BOTTOM,(self.x,self.bottom))
+
+    def colide(self,bird):
+        bird_mask = bird.getget_mask()
+        top_mask = pygame.mask.from_surface(self.PIPE_TOP)
+        bottom_mask = pygame.mask.from_surface(self.PIPE_BOTTOM)
+
+        top_offset = (self.x-bird.x,self.top - round(bird.y))
+        bottom_offset = (self.x-bird.x,self.bottom  - round(bird.y))
+
+        b_point = bird_mask.overlap(bottom_mask,bottom_offset)
+        t_point  = bird_mask.overlap(top_mask,top_offset)
+
+        if t_point or b_point:
+            return True
+
+        return False
+
+
+class Base:
     
 
 def draw_win(win,bird):
